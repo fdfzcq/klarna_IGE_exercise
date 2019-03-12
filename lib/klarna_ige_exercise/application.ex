5634adbb381e85c1c70230d64e22b7f100814313
fdfzcq <fdfzcq@gmail.com>
2019-03-12 19:01:35 +0100

    Clean up ecto

--- lib/klarna_ige_exercise/application.ex
+++ lib/klarna_ige_exercise/application.ex
@@ -1,29 +1,16 @@
 defmodule KlarnaIGEExercise.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Start the Ecto repository
-      KlarnaIGEExercise.Repo,
-      # Start the endpoint when the application starts
       KlarnaIGEExerciseWeb.Endpoint
-      # Starts a worker by calling: KlarnaIGEExercise.Worker.start_link(arg)
-      # {KlarnaIGEExercise.Worker, arg},
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KlarnaIGEExercise.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   def config_change(changed, _new, removed) do
     KlarnaIGEExerciseWeb.Endpoint.config_change(changed, removed)
     :ok
--- lib/klarna_ige_exercise/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule KlarnaIGEExercise.Repo do
-  use Ecto.Repo,
-    otp_app: :klarna_ige_exercise,
-    adapter: Ecto.Adapters.Postgres
-end
